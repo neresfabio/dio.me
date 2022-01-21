80a6ee063c3243c11e6149dbb74f7f86232c27b3
neres <neresfjcomunic@gmail.com>
2022-01-21 20:14:26 -0300

    Tela com detalhes.

---
+++ matches-simulador-app/Simulador/app/src/main/java/me/dio/myapplication/DetailActivity.kt
@@ -0,0 +1,19 @@
+package me.dio.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.dio.myapplication.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
